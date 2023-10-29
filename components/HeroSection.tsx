13d7e773a1dcb9af080fe0f1520c8ee135fd3c33
Sami <121911966+sami23ibrahim@users.noreply.github.com>
2023-10-28 20:01:27 -0400

    4

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -81,7 +81,7 @@ const HeroSection = () => {
           offset={-100}
           duration={500}
         >
-          <HiArrowDown size={35} className="animate-bounce" />
+          <HiArrowDown size={35} className="animate-bounce hidden md:inline-block" />
         </Link>
       </div>
     </section>
